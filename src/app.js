56fefc5248a7e0cabe8ce8e51ed0c59d580d9097
Alexis Sayago <as.sayagom@gmail.com>
2023-04-07 13:46:45 +0000

    exercise added

--- src/app.js
+++ src/app.js
@@ -2,10 +2,31 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function excuses() {
+  var excuse =
+    who[Math.floor(Math.random() * who.length)] +
+    " " +
+    action[Math.floor(Math.random() * action.length)] +
+    " " +
+    what[Math.floor(Math.random() * what.length)] +
+    " " +
+    when[Math.floor(Math.random() * when.length)];
+
+  return excuse;
+}
+
+var excuseTag = document.getElementById("excuse");
+excuseTag.textContent = excuses();
